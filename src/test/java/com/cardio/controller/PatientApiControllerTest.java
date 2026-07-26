d5afec65d38a0950440af9ded7c9536224273ab5
Azriel408 <anhldhe182061@fpt.edu.vn>
2026-07-26 13:47:50 +0700

    fix: Update PatientApiControllerTest for Spring Boot 3.x compatibility

--- src/test/java/com/cardio/controller/PatientApiControllerTest.java
+++ src/test/java/com/cardio/controller/PatientApiControllerTest.java
@@ -23,7 +23,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(controllers = PatientApiController.class, secure = false)
+@WebMvcTest(controllers = PatientApiController.class)
 class PatientApiControllerTest {
 
     @Autowired
